5509c86d89ec70e8e702c3636349505e60fd17e3
ahmad rafi wirana <ahmad.rafi02@ui.ac.id>
2022-03-29 15:37:21 +0700

    finishing footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,1 +1,12 @@
-export {};
+/* eslint-disable react/no-unknown-property */
+
+
+export default function Footer(props: Any){
+  return (
+    <footer class="px-6 md:px-20 lg:px-48 xl:px-60 2xl:px-96 py-8 flex justify-center items-center absolute inset-x-0 bottom-0 bg-gray-50 dark:bg-primary">
+      <h1 class ="font-sans font-semibold text-s md:text-lg">
+        Developed by Ahmad Rafi Wirana - 2022
+      </h1>
+    </footer>
+  );
+}
--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,11 +1,13 @@
 import {Fragment} from 'react';
 import MainNavigation from './main-navigation';
+import Footer from './footer';
 
 export default function Layout(props: Any){
   return (
     <Fragment>
       <MainNavigation />
       <main>{props.children}</main>
+      <Footer/>
     </Fragment>
   );
 }
